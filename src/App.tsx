fd0effa72ac567157398392345d1c8c6c7fc8fdb
Your Name <jpbcatindig@outlook.com>
2025-10-02 22:02:53 +0800

    Remove basename from BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={import.meta.env.DEV ? '/' : '/imaxpixel-neo-signature'}>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
